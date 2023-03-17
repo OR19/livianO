bd8ab9bccac13acad597b39cf9732210963c28df
Orlando Martínez <orlandomc19@gmail.com>
2023-03-17 02:19:44 -0600

    feat: :sparkles: Clase Response
    Proporciona funcionalidades básicas para dar respuesta

---
+++ src/Core/Response.php
@@ -0,0 +1,40 @@
+<?php
+namespace Liviano\Core;
+/**
+ * Ofrece operaciones básicas para ofrecer respuestas
+ * @author Orlando Martínez
+ */
+class Response {
+    /**
+     * Establece el código HTTP
+     * @param int $code Código de estado HTTP
+     */
+    public function sendHttpCode( int $code ): void {
+        http_response_code($code);
+    }
+    /**
+     * Realiza una redirección a una URL
+     * @param string $url Dirección URL
+     * @param int $statusCode [Opcional] Código de estado HTTP
+     */
+    public function redirect(string $url, int $statusCode = 303): void {
+       header("Location: $url", true, $statusCode);
+    }
+    /**
+     * Establece la cabecera Content-Type: application/json; charset=utf-8
+     */
+    public function headerApplicationJson(): void {
+        header('Content-Type: application/json; charset=utf-8');
+    }
+    /**
+     * Realiza un echo de un json como respuesta
+     * @param array $json Arreglo JSON a aplicar json_encode
+     * @param int $statusCode [Opcional] Código de estado de la respuesta
+     */
+    public function sendJSON( array $json, int $statusCode = 200 ): void {
+        http_response_code($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode( $json );
+    }
+}
+?>
